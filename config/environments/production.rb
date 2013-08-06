b55e54fde251fea2cd77cf164e6e8e9e85fd4c6e
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2013-08-06 21:22:54 +0200

    modificado archivo de configuracion de produccion para que se compilen os assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Sex::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.initialize_on_precompile = true
+  
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
