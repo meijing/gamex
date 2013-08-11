ea99470e957c40b70a2742cc762187889bc3b212
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2013-08-11 12:46:37 +0200

    join para recuperar o xoguete tamen a base da accion

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -4,7 +4,7 @@ class Toy < ActiveRecord::Base
    has_many :toy_zones
    
   scope :by_zone, lambda { |zone_id, act_id|
-    joins(:toy_zones).where('toy_zones.erogenous_zone_id = ? and act_id=?', zone_id, act_id) unless zone_id.nil?
+    joins(:toy_zones).where('toy_zones.erogenous_zone_id = ?', zone_id).joins(:toy_acts).where('toy_acts.act_id = ?', act_id) unless zone_id.nil? and act_id.nil?
   }
   
   def self.get_toy_random(zone_id, act_id)
