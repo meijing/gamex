f4bcc05142f55961fabbe5214887107afcf6b06b
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2013-09-01 18:00:32 +0200

    incluidos os gustos dos xogadores

--- app/models/zone_act.rb
+++ app/models/zone_act.rb
@@ -4,7 +4,7 @@ class ZoneAct < ActiveRecord::Base
    belongs_to :erogenous_zone
    
   scope :by_zone, lambda { |zone_id, acts|
-    joins(:erogenous_zone).where('(zone_acts.sex = erogenous_zones.sex or zone_acts.sex = 2) and zone_acts.erogenous_zone_id = ? and zone_acts.act = ?', zone_id, acts) unless zone_id.nil?
+    joins(:erogenous_zone).where('(zone_acts.sex = erogenous_zones.sex or zone_acts.sex = 2) and zone_acts.erogenous_zone_id = ? and zone_acts.act_id in (?)', zone_id, acts) unless zone_id.nil?
   }
   
   def self.get_act_random(zone_id, acts)
