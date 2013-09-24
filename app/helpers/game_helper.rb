1d881f509fdff790815e82929194f417392ab3e2
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2013-09-24 18:57:53 +0200

    diseño adaptado os moviles

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,16 +1,18 @@
 module GameHelper
   def get_sex_p1(sex, image, value)
-    if (sex == value.to_s)
-      return content_tag( :div, link_to(image_tag(image, :size=>"80x80"), select_p1_path(:sex_p_1=>value)), :class=>'span5 proba_fondo9') 
-    end
-    return content_tag( :div, link_to(image_tag(image, :size=>"80x80"), select_p1_path(:sex_p_1=>value)), :class=>'span5 proba_fondo7') 
+    content_tag( :span, link_to(image_tag(image, :size=>"35x35"), select_p1_path(:sex_p_1=>value)), :class=>(sex == value.to_s)?'span1 proba_fondo9':'span1 proba_fondo7') 
+#  if (sex == value.to_s)
+#    return content_tag( :div, link_to(image_tag(image, :size=>"40x40"), select_p1_path(:sex_p_1=>value)), :class=>'span1 proba_fondo9') 
+#  end
+#  return content_tag( :div, link_to(image_tag(image, :size=>"40x40"), select_p1_path(:sex_p_1=>value)), :class=>'span1 proba_fondo7') 
   end
   
   def get_sex_p2(sex, image, value)
-    if (sex == value.to_s)
-      return content_tag( :div, link_to(image_tag(image, :size=>"80x80"), select_p2_path(:sex_p_2=>value)), :class=>'span5 proba_fondo9') 
-    end
-    return content_tag( :div, link_to(image_tag(image, :size=>"80x80"), select_p2_path(:sex_p_2=>value)), :class=>'span5 proba_fondo7') 
+    content_tag( :span, link_to(image_tag(image, :size=>"35x35"), select_p2_path(:sex_p_2=>value)), :class=>(sex == value.to_s)?'span1 proba_fondo9':'span1 proba_fondo7') 
+#    if (sex == value.to_s)
+#      return content_tag( :div, link_to(image_tag(image, :size=>"40x40"), select_p2_path(:sex_p_2=>value)), :class=>'span1 proba_fondo9') 
+#    end
+#    return content_tag( :div, link_to(image_tag(image, :size=>"40x40"), select_p2_path(:sex_p_2=>value)), :class=>'span1 proba_fondo7') 
   end
   
   def get_image_by_sex(sex)
